95cdf5bccee2b1882f4c5f41c9fda30ababb8012
Rafal Kukuczka <rafal@pkey.pl>
2014-05-04 11:24:27 +0200

    moj komit

---
+++ reove.bat
@@ -0,0 +1,5 @@
+rem Ta linia sie nie wykona
+echo 'Usuwanie plikow'
+
+FOR %%A IN (2 3 5 7) DO del readme.%%A
+
---
+++ test.bat
@@ -0,0 +1,5 @@
+
+echo 'Kopiowanie plikow'
+
+FOR %%A IN (2 3 5 7 ) DO copy readme.txt readme.%%A
+
